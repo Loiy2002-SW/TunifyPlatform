183757dc0886d18f5212057c6bd339162ed407b8
loiy <lloiy9310@gmail.com>
2024-08-08 12:22:37 +0300

    Add services

---
+++ TunifyPlatform/Repositories/Services/SongRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TunifyPlatform.Data;
+using TunifyPlatform.Models;
+using TunifyPlatform.Repositories.Interfaces;
+
+namespace TunifyPlatform.Repositories.Services
+{
+    public class SongRepository : ISongRepository
+    {
+        private readonly TunifyDbContext _context;
+
+        public SongRepository(TunifyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Song>> GetAllSongsAsync()
+        {
+            return await _context.Songs.ToListAsync();
+        }
+
+        public async Task<Song> GetSongByIdAsync(int id)
+        {
+            return await _context.Songs.FindAsync(id);
+        }
+
+        public async Task AddSongAsync(Song song)
+        {
+            await _context.Songs.AddAsync(song);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateSongAsync(Song song)
+        {
+            _context.Songs.Update(song);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteSongAsync(int id)
+        {
+            var song = await _context.Songs.FindAsync(id);
+            if (song != null)
+            {
+                _context.Songs.Remove(song);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
